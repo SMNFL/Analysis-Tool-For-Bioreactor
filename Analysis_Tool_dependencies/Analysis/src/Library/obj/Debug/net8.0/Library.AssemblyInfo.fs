a1dbee95f05fb3ded6bc886fc593866c5e154e26
simonfoellinger <simonfoellinger@web.de>
2024-11-05 22:53:31 +0100

    clear output folder

--- Analysis_Tool_dependencies/Analysis/src/Library/obj/Debug/net8.0/Library.AssemblyInfo.fs
+++ Analysis_Tool_dependencies/Analysis/src/Library/obj/Debug/net8.0/Library.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42022d79c891af4834e440d4863f75a174b3dbcb")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ffb6e14606c9002cb154e64cc5102c2306cc0ad")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
