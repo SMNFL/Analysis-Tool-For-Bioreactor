45cda01be915794d19f05a61b115df448940576d
simonfoellinger <simonfoellinger@web.de>
2024-11-05 14:17:03 +0100

    change MacOS .app

--- Analysis_Tool_dependencies/Analysis/src/Library/obj/Debug/net8.0/Library.AssemblyInfo.fs
+++ Analysis_Tool_dependencies/Analysis/src/Library/obj/Debug/net8.0/Library.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3c22b6af668f695ae6cdd59eef1fce577bc064f")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b6abc336d71e81e51e3dc0868b679bce0d3d8c0")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
